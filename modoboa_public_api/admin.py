0415079530393a8d80b3300bf8049ccd7d5a2100
Antoine Nguyen <tonio@ngyn.org>
2016-06-15 15:17:16 +0200

    Improve admin. display

--- modoboa_public_api/admin.py
+++ modoboa_public_api/admin.py
@@ -4,15 +4,14 @@ from .models import ModoboaInstance, ModoboaExtension
 
 
 class ModoboaInstanceAdmin(admin.ModelAdmin):
-
-    """
-    Admin class for ModoboaInstance model.
-    """
+    """Admin class for ModoboaInstance model."""
 
     list_display = (
         "ip_address", "hostname", "known_version", "last_request",
         "domain_counter", "mailbox_counter", "alias_counter"
     )
+    list_filter = ("known_version", )
+    search_fields = ["ip_address", "hostname"]
 
 
 class ModoboaExtensionAdmin(admin.ModelAdmin):
