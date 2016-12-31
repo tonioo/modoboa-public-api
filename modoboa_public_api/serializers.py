40cb3c5a87ff3f050c6a7a93b36f168aaf121fe5
Antoine Nguyen <tonio@ngyn.org>
2016-12-31 12:08:18 +0100

    Use a distinct() constraint.

--- modoboa_public_api/serializers.py
+++ modoboa_public_api/serializers.py
@@ -64,7 +64,7 @@ class InstanceSerializer(serializers.ModelSerializer):
         """Fetch and set extensions."""
         extensions = [extension.replace("_", "-") for extension in extensions]
         extensions = models.ModoboaExtension.objects.filter(
-            name__in=extensions)
+            name__in=extensions).distinct()
         for extension in extensions:
             instance.extensions.add(extension)
 
